296645f290d48aa44a67dfa1b140b7f4a435abc2
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-04-24 22:38:38 +0700

    Test story board

---
+++ SimpleLogin/IndexViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace SimpleLogin
+{
+    public partial class IndexViewController : UIViewController
+    {
+        public IndexViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ SimpleLogin/ViewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using UIKit;
+
+namespace SimpleLogin
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController (IntPtr handle) : base (handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			LoginButton.TouchUpInside += delegate {
+				var controller = Storyboard.InstantiateViewController("IndexViewController") as IndexViewController;
+				PresentViewController (controller, true, null);
+			};
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
